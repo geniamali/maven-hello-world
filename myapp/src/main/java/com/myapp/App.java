69b106229d48698facde01949a77640b7d1a7d79
Genia Malinsky <geniam@onvego.com>
2024-07-19 13:41:39 +0300

    changed the msg again

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! \n\n Evgeny Malinsky" );
+        System.out.println( "Hello World!\nEvgeny Malinsky" );
     }
 }
